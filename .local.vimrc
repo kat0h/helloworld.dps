2b2e89ca7ecbd76fb7033dc14556d45da5c774b1
KotaKato <peony.btn@gmail.com>
2021-04-19 23:52:50 +0900

    test

---
+++ .local.vimrc
@@ -0,0 +1,2 @@
+set runtimepath^=~/dev/vim-plugins/helloworld.dps
+let g:denops#script#typecheck = 1
---
+++ denops/helloworld/mod.ts
@@ -0,0 +1,7 @@
+import { start } from "https://deno.land/x/denops_std@v0.2/mod.ts";
+
+start(async (vim) => {
+  // ここにプラグインの初期化処理を記載する
+  console.log("Hello Denops!");
+});
+
